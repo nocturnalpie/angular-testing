1dca6715ee501e50fd487db5ad8e334a728a2e74
nocturnalpie <nicole@theliveseys.net>
2022-03-30 11:38:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ht5q9a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ht5q9a)
---
+++ src/app/tooltip-overview-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic tooltip
+ */
+@Component({
+  selector: 'tooltip-overview-example',
+  templateUrl: 'tooltip-overview-example.html',
+})
+export class TooltipOverviewExample {}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
