14f6cd97f2331a79a6d4572ceff59c013f94e3a8
Nicole Livesey <nicole.livesey@microlise.com>
2022-03-31 10:46:36 +0100

    Updated test comments

--- src/app/waitForAsync/async.service.spec.ts
+++ src/app/waitForAsync/async.service.spec.ts
@@ -9,17 +9,19 @@ describe('AsyncService', () => {
 		service = TestBed.inject(AsyncService);
 	});
 
-	it('should pass with waitForAsync', waitForAsync(() => {
-		service.asynchronousMethod().then(() => {
-			expect(true).toBeTruthy();
-		});
-	}));
-
 	it('should fail with waitForAsync', () => {
 		service.asynchronousMethod().then(() => {
 			/** This line isn't reached by the tests because the test
-			 * finished before this asynchronous thread got to this point*/
+			 * finished before this asynchronous thread got to this point */
 			expect(true).toBeTruthy();
 		});
 	});
+
+	it('should pass with waitForAsync', waitForAsync(() => {
+		service.asynchronousMethod().then(() => {
+			/** Now we have waitForAsync the test waits for this thread
+			 * to complete before it ends the test so this line is reached */
+			expect(true).toBeTruthy();
+		});
+	}));
 });
