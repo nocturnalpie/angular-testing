67febb4472ce5902efc2a0dfc38ce9643afe8484
Nicole Livesey <nicole.livesey@microlise.com>
2022-03-31 10:53:40 +0100

    Added router testing module to stop wallaby complaining

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [AppModule]
+			imports: [
+				AppModule,
+				RouterTestingModule
+			]
 		}).compileComponents();
 	});
 
