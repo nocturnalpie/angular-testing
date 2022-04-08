5185ae4820a4ec4508fab8d257b3560c50e1019e
Nicole Livesey <nicole.livesey@microlise.com>
2022-04-08 15:57:09 +0100

    Added comment about passing in properties

--- src/app/mocking-and-spying/user.component.mock.spec.ts
+++ src/app/mocking-and-spying/user.component.mock.spec.ts
@@ -16,7 +16,10 @@ describe('UserComponent with Mock UserService', () => {
 		/** Create your mock.
 		 *  Note: Remember to define it in the before each so that any changes you make to it
 		 *  don't affect the other tests. */
-		mockUserService = jasmine.createSpyObj<UserService>({ getAllUsers: of(fakeUsers) });
+		mockUserService = jasmine.createSpyObj<UserService>(
+			{ getAllUsers: of(fakeUsers) }
+			/*, { property1: 1, property2: 2 }*/
+		); // <-- You can also pass in properties here
 
 		/** You may also this this written with the class as the first parameter but we prefer
 		 *  the first method as it helps with renaming and intellisense etc */
