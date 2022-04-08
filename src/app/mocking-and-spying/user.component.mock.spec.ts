639a34de3536f300aa3ad58b64b4b08857d1f35a
Nicole Livesey <nicole.livesey@microlise.com>
2022-04-08 16:00:10 +0100

    fixed comments

--- src/app/mocking-and-spying/user.component.mock.spec.ts
+++ src/app/mocking-and-spying/user.component.mock.spec.ts
@@ -13,20 +13,17 @@ describe('UserComponent with Mock UserService', () => {
 	const fakeUsers = ['user1', 'user2'];
 
 	beforeEach(async () => {
-		/** Create your mock.
-		 *  Note: Remember to define it in the before each so that any changes you make to it
-		 *  don't affect the other tests. */
+		// Create your mock.
+		// Note: Remember to define it in the before each so that any changes you make to it don't affect the other tests.
 		mockUserService = jasmine.createSpyObj<UserService>(
 			{ getAllUsers: of(fakeUsers) }
 			//, { property1: 1, property2: 2 }   <-- You can also pass in properties here
 		);
 
-		/** You may also this this written with the class as the first parameter but we prefer
-		 *  the first method as it helps with renaming and intellisense etc */
+		// You may also this this written with the class as the first parameter but we prefer the first method as it helps with renaming and intellisense etc
 		// mockUserService = jasmine.createSpyObj('UserService', { getAllUsers: of(fakeUsers) });
 
-		/** If you don't need the mock to return anything or you want to set it to something
-		 *  different in each test then you can just pass the method names in like this instead. */
+		// If you don't need the mock to return anything or you want to set it to something different in each test then you can just pass the method names in like this instead.
 		// mockUserService = jasmine.createSpyObj<UserService>(['getAllUsers']);
 
 		await TestBed.configureTestingModule({
